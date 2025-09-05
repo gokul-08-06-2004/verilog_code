5643f6ab30c4d122d41bed99e582edbf1a37cb58
Gokul S <sarangokul86@gmail.com>
2025-09-05 23:47:40 +0530

    Update question_1.v

--- Assignment/assignment_2/question_1.v
+++ Assignment/assignment_2/question_1.v
@@ -1,2 +1,13 @@
-c
+//1. Design a 2-bit comparator using basic gates (AND, OR, NOT) to compare two 2-bit
+//numbers A and B. The output should indicate if A == B, A > B, or A < B.
 
+module comparator_2bit(input a,b,output x,y,z);
+
+wire w1,w2; 
+not(w1,b);
+not(w2,a);
+xnor(x,a,b);
+and(y,a,w1);
+and(z,w2,b);
+
+endmodule
