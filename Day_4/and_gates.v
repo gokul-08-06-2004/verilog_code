d074ff8621d0097a9dc889c0459b83b3ad97263f
Gokul S <sarangokul86@gmail.com>
2025-09-09 20:52:25 +0530

    Add files via upload

---
+++ Day_4/and_gates.v
@@ -0,0 +1,9 @@
+module and_gate(input a,b,output reg y);
+
+always@(*)begin
+if(a&b)
+y=1;
+else
+y=0;
+end
+endmodule
---
+++ Day_4/xor_gate.v
@@ -0,0 +1,8 @@
+module xor_gate(input a,b,output reg y);
+always @(*)begin
+if(a^b)
+y=1; 
+else
+y=0;
+end
+endmodule 
